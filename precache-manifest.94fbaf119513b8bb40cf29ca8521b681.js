112e62d74a48e1c614330f560d6af2a8ff133a26
Arnab Dutta <arnabduttasocial@gmail.com>
2021-07-28 16:34:40 +0530

    Updates

---
+++ precache-manifest.94fbaf119513b8bb40cf29ca8521b681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab57223f561a084eedf21f79826a69df",
+    "url": "/track-covid/index.html"
+  },
+  {
+    "revision": "c18529df81f6cd038dcf",
+    "url": "/track-covid/static/css/2.15f857eb.chunk.css"
+  },
+  {
+    "revision": "a4e955779ef6237f9fee",
+    "url": "/track-covid/static/css/main.be4f3d85.chunk.css"
+  },
+  {
+    "revision": "c18529df81f6cd038dcf",
+    "url": "/track-covid/static/js/2.d7384f35.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/track-covid/static/js/2.d7384f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4e955779ef6237f9fee",
+    "url": "/track-covid/static/js/main.e1bfe99f.chunk.js"
+  },
+  {
+    "revision": "60f3cef68dc8e88d2db5",
+    "url": "/track-covid/static/js/runtime-main.7c8931d9.js"
+  }
+]);
